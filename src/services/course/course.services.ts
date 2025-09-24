da0fcef817819c368ca4541ff1eda36f2b2b7e43
samio11 <samiohasan6@gmail.com>
2025-09-24 14:50:06 +0600

    making service api for course

---
+++ src/services/course/course.services.ts
@@ -0,0 +1,58 @@
+import { revalidateTag } from "next/cache";
+
+export const createCourse = async (payload: FormData) => {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND}/course/create`,
+      {
+        method: "POST",
+        body: payload,
+      }
+    );
+    revalidateTag("course");
+    const result = await res.json();
+    return result;
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const getAllCourse = async () => {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND}/course`, {
+      method: "GET",
+      next: {
+        tags: ["course"],
+      },
+    });
+    const result = await res.json();
+    return result;
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const getACourse = async (id: string) => {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND}/course/${id}`, {
+      method: "GET",
+    });
+    const result = await res.json();
+    return result;
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const deleteACourse = async (id: string) => {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND}/course/${id}`, {
+      method: "DELETE",
+    });
+    revalidateTag("course");
+    const result = await res.json();
+    return result;
+  } catch (err) {
+    throw err;
+  }
+};
---
+++ src/type/course.interface.ts
@@ -0,0 +1,6 @@
+export interface ICourse {
+  title: string;
+  desc: string;
+  imageUrl: string;
+  videoUrl: string;
+}
